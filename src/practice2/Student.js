9f4dcd37e1d5decbfd7edf118326fb393e635141
yuan qihang <qihang.yuan@thoughtworks.com>
2020-11-11 23:27:47 +0800

    complete test2

--- src/practice2/Student.js
+++ src/practice2/Student.js
@@ -1,1 +1,14 @@
-export class Student {}
+import { Person } from "./Person";
+
+export class Student extends Person {
+  constructor(name, className) {
+    super(name);
+    this.className = className;
+    this.study = this.study.bind(this);
+  }
+
+  study() {
+    this.move();
+    console.log(`${this.name} is studying in class1`);
+  }
+}
